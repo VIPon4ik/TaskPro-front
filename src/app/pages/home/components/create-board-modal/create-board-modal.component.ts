4508c0a49fe460cc2b65f846c044104ef90594ed
VIPon4ik <umniyd@gmail.com>
2024-05-22 18:57:02 +0300

    create board modal

--- src/app/pages/home/components/create-board-modal/create-board-modal.component.ts
+++ src/app/pages/home/components/create-board-modal/create-board-modal.component.ts
@@ -1,15 +1,24 @@
-import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { DashboardBackgrounds, DashboardIcons } from '@pages/home/models';
 import { ButtonComponent } from '@shared/ui/components/button/button.component';
 import { InputComponent } from '@shared/ui/components/input/input.component';
 import { ButtonColor } from '@shared/ui/models';
 import { GetFormControlPipe } from '@shared/ui/pipes/get-form-control.pipe';
+import { tap } from 'rxjs';
 
+@UntilDestroy()
 @Component({
   selector: 'tp-create-board-modal',
   standalone: true,
-  imports: [InputComponent, ButtonComponent, GetFormControlPipe],
+  imports: [
+    InputComponent,
+    ButtonComponent,
+    GetFormControlPipe,
+    ReactiveFormsModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './create-board-modal.component.html',
 })
 export class CreateBoardModalComponent implements OnInit {
@@ -17,8 +26,10 @@ export class CreateBoardModalComponent implements OnInit {
 
   dashboardForm!: FormGroup;
   ButtonColor = ButtonColor;
+  DashboardIcons = DashboardIcons;
 
   private formBuilder = inject(FormBuilder);
+  private cdr = inject(ChangeDetectorRef);
 
   get icons(): string[] {
     return Object.values(DashboardIcons);
@@ -30,14 +41,25 @@ export class CreateBoardModalComponent implements OnInit {
 
   ngOnInit(): void {
     this.setupForm();
+    this.handleValueChanges();
   }
 
   setupForm(): void {
     this.dashboardForm = this.formBuilder.group({
       title: [''],
-      icon: [''],
-      background: [''],
+      icon: [DashboardIcons.Project],
+      background: [DashboardBackgrounds.NoBg],
     });
   }
 
+  handleValueChanges(): void {
+    this.dashboardForm.valueChanges
+      .pipe(
+        tap(() => {
+          console.log(this.dashboardForm.value);this.cdr.detectChanges();
+        }),
+        untilDestroyed(this),
+      )
+      .subscribe();
+  }
 }
